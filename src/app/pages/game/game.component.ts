50f1e8f08dd01015e87f30e4a539933198bcec06
Jon <artolajon2@gmail.com>
2021-04-13 22:58:08 +0200

    Delete hidden lines

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cell } from 'src/app/models/cell';
 
+const LINE_LENGTH = 9;
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
@@ -39,6 +40,7 @@ export class GameComponent implements OnInit {
       if (option != null){
         this.list[this.selected].visible=false;
         this.list[option].visible=false;
+        this.checkLines(this.selected, option);
       }
       this.selected = undefined;
       this.options = undefined;
@@ -59,7 +61,7 @@ export class GameComponent implements OnInit {
   }
 
   checkUp(selected: number){
-    for(let i = selected - 9; i >= 0; i -= 9){
+    for(let i = selected - LINE_LENGTH; i >= 0; i -= LINE_LENGTH){
       let done = this.check(selected, i);
       if (done)
         break;
@@ -84,7 +86,7 @@ export class GameComponent implements OnInit {
   }
 
   checkDown(selected: number){
-    for(let i = selected + 9; i < this.list.length; i += 9){
+    for(let i = selected + LINE_LENGTH; i < this.list.length; i += LINE_LENGTH){
       let done = this.check(selected, i);
       if (done)
         break;
@@ -108,6 +110,30 @@ export class GameComponent implements OnInit {
     this.list.filter(c=> c.visible).forEach(cell => {
       this.list.push(new Cell(cell.type))
     });
+  }
+
+
+  checkLines(cell1: number, cell2: number){
+    let line1Start = cell1 - cell1 % LINE_LENGTH;
+    let line2Start = cell2 - cell2 % LINE_LENGTH;
+    if (line1Start == line2Start){
+      this.checkLine(line1Start);
+    }else{
+      let smallest = line1Start < line2Start ? line1Start : line2Start;
+      let biggest = line1Start > line2Start ? line1Start : line2Start;
+      this.checkLine(biggest);
+      this.checkLine(smallest);
+    }
+
+  }
+
+
+  checkLine(lineStart: number){
+    let lineEnd = lineStart + LINE_LENGTH;
+    let line = this.list.slice(lineStart, lineEnd);
+    if (!line.some(c=> c.visible)){
+      this.list.splice(lineStart, LINE_LENGTH);
+    }
   }
 
 
